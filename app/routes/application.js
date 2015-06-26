bb731c1303401a8b0d91465786b1525312092ec7
John Hampton <john_j_hampton@yahoo.com>
2015-06-26 12:41:40 -0400

    Refactor application route promise slightly

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,21 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return new Ember.RSVP.Promise(function(resolve) {
-      var userLocation = {};
-      if ('geolocation' in navigator) {
+    if ('geolocation' in navigator) {
+      return new Ember.RSVP.Promise(function(resolve) {
+        var userLocation = {};
         navigator.geolocation.getCurrentPosition(function(position) {
-          console.log("initial lat", position.coords.latitude);
-          console.log("initial long", position.coords.longitude);
           userLocation.latitude = position.coords.latitude;
           userLocation.longitude = position.coords.longitude;
-          console.log("userLocation object is", userLocation);
           resolve(userLocation);
         });
-      }
-      else {
-        alert("Oops, no geolocation support");
-      }
-    });
+      });
+    }
   }
 });
