e7f768aa458ba171337dd96d057306cf5282380e
John Hampton <john_j_hampton@yahoo.com>
2015-06-26 10:32:44 -0400

    Create static mockup for landing/request  and forecast application states

--- Brocfile.js
+++ Brocfile.js
@@ -17,7 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/skeleton/css/skeleton.css', { destDir: 'css' });
+app.import('bower_components/skeleton/css/skeleton.css');
 
 module.exports = app.toTree();
 
